e837d3dddcd8e74c95d691aec18a62ce01fdd72e
electrovir <electrovir@users.noreply.github.com>
2022-05-02 15:20:15 +0000

    add toniq-icon to package exports

--- src/scripts/publishing.ts
+++ src/scripts/publishing.ts
@@ -1,3 +1,11 @@
+/*
+    This file contains pre and post install scripts that are complicated enough that inlining them
+    in package.json in a cross-platform-compatible manner is not feasible.
+    
+    Currently these scripts remove and re-insert a preinstall npm script which is needed to run
+    "npm install" locally in this repo, but causes issues when another packages tries to install
+    this package from npm (via "npm install @toniq-labs/design-system").
+*/
 import {runShellCommand} from 'augment-vir/dist/node-only';
 import {readFile, writeFile} from 'fs/promises';
 import {dirname, join, relative} from 'path';
