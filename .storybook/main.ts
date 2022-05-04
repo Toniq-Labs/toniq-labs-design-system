bea2a57e334904d0c2e979f0bf97196ca1559296
electrovir <electrovir@users.noreply.github.com>
2022-05-04 13:28:13 +0000

    modify configs and improve build chunking

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,37 @@
 import {StorybookConfig} from '@storybook/core-common';
 
-const storybookConfig: StorybookConfig = {
-    stories: [
-        '../src/**/*.stories.mdx',
-        '../src/**/*.stories.@(js|jsx|ts|tsx)',
-    ],
-    addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/addon-interactions',
-    ],
-    framework: '@storybook/react',
+export const stories: StorybookConfig['stories'] = [
+    '../src/**/*.stories.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+];
+export const addons: StorybookConfig['addons'] = [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+];
+export const framework: StorybookConfig['framework'] = '@storybook/react';
+
+const maxAssetSize = 1024 * 1024;
+
+export const webpackFinal: StorybookConfig['webpackFinal'] = (config) => {
+    config.optimization = {
+        ...config.optimization,
+        splitChunks: {
+            ...config.optimization?.splitChunks,
+            chunks: 'all',
+            minSize: 30 * 1024,
+            maxSize: maxAssetSize,
+        },
+    };
+    config.performance = {
+        ...config.performance,
+        maxAssetSize: maxAssetSize,
+    };
+
+    return config;
 };
 
-export default storybookConfig;
+export const features: StorybookConfig['features'] = {
+    /** Decrease bundle size */
+    storyStoreV7: true,
+};
