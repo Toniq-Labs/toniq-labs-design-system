4689f5d7d12783c98b0ad496359ad137befc8437
electrovir <electrovir@users.noreply.github.com>
2022-05-07 13:36:42 +0000

    don't use rollup to produce output

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,29 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import {dirname} from 'path';
 import dts from 'rollup-plugin-dts';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import scss from 'rollup-plugin-scss';
 import {terser} from 'rollup-plugin-terser';
-
-const packageJson = require('./package.json');
+import packageJson from './package.json';
 
 export default [
     {
         input: 'src/index.tsx',
         output: [
             {
-                file: packageJson.main,
+                dir: dirname(packageJson.main),
                 format: 'cjs',
                 sourcemap: true,
+                preserveModules: true, // Keep directory structure and files
             },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: true,
-            },
+            // {
+            //     dir: dirname(packageJson.module),
+            //     format: 'esm',
+            //     sourcemap: true,
+            //     preserveModules: true, // Keep directory structure and files
+            // },
         ],
         plugins: [
             peerDepsExternal(),
