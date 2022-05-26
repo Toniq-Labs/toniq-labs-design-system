3038e7f19b44c21555658a174c65f9e677557eef
ponnexcodev <emmanuelr@codev.com>
2022-05-26 16:12:53 +0800

    Added test

--- src/elements/toniq-dropdown/toniq-dropdown.test.ts
+++ src/elements/toniq-dropdown/toniq-dropdown.test.ts
@@ -1,9 +1,62 @@
-import {assert} from '@open-wc/testing';
+import {assert, expect, fixture} from '@open-wc/testing';
+import {assign, html} from 'element-vir';
+import {queryThroughShadow} from '../../element-testing/query-through-shadow';
 import {ToniqDropdown} from './toniq-dropdown.element';
 
 describe(ToniqDropdown.tagName, () => {
+    const list = [
+        {
+            value: 1,
+            label: 'Option 1',
+        },
+        {
+            value: 2,
+            label: 'Option 2',
+        },
+        {
+            value: '3',
+            label: 'Option 3',
+        },
+    ];
+
     it('should be registered as a custom element', () => {
         const newlyCreated = document.createElement(ToniqDropdown.tagName);
         assert.instanceOf(newlyCreated, ToniqDropdown);
+    });
+
+    it('should correctly set default option', async () => {
+        const rendered = await fixture(
+            html`
+                <${ToniqDropdown} ${assign(ToniqDropdown.props.list, list)} />
+            `,
+        );
+
+        const selectedOption = queryThroughShadow('span.select-selected', rendered);
+        expect(selectedOption).to.have.text(list[0]!.label);
+    });
+
+    it('should correctly set selected default option', async () => {
+        const rendered = await fixture(
+            html`
+                <${ToniqDropdown}
+                    ${assign(ToniqDropdown.props.list, list)}
+                    ${assign(ToniqDropdown.props.select, list[2])}
+                />
+            `,
+        );
+
+        const selectedOption = queryThroughShadow('span.select-selected', rendered);
+        expect(selectedOption).to.have.text(list[2]!.label);
+    });
+
+    it('should display the correct number of options', async () => {
+        const rendered = await fixture(
+            html`
+                <${ToniqDropdown} ${assign(ToniqDropdown.props.list, list)} />
+            `,
+        );
+
+        const options = queryThroughShadow('div.select-options', rendered);
+        expect(options?.childElementCount).equals(list.length);
     });
 });
