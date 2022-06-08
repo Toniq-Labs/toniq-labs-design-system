5b95c0cd150ec0c4c65b744c016e0e43d702da1c
ponnexcodev <emmanuelr@codev.com>
2022-06-08 09:01:45 +0800

    Removed toniq-slider

--- src/elements/toniq-slider/toniq-slider.test.ts
+++
@@ -1,9 +0,0 @@
-import {assert} from '@open-wc/testing';
-import {ToniqSlider} from './toniq-slider.element';
-
-describe(ToniqSlider.tagName, () => {
-    it('should be registered as a custom element', () => {
-        const newlyCreated = document.createElement(ToniqSlider.tagName);
-        assert.instanceOf(newlyCreated, ToniqSlider);
-    });
-});
