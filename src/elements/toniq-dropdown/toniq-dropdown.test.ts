5a44a09b46840bf28c2b382b3681396e5832e3aa
electrovir <electrovir@users.noreply.github.com>
2022-06-11 18:40:31 +0000

    update dropdown tests

--- src/elements/toniq-dropdown/toniq-dropdown.test.ts
+++ src/elements/toniq-dropdown/toniq-dropdown.test.ts
@@ -1,5 +1,7 @@
-import {expect, fixture} from '@open-wc/testing';
+import {assert, fixture} from '@open-wc/testing';
 import {assign, html} from 'element-vir';
+import {assertInstanceOf} from '../../element-testing/assertion-helpers';
+import {createFixtureTest} from '../../element-testing/fixture-test';
 import {queryThroughShadow} from '../../element-testing/query-through-shadow';
 import {createElementRegistrationTest} from '../../element-testing/test-element-creation';
 import {createFocusTests} from '../../element-testing/test-focus';
@@ -23,49 +25,62 @@ describe(ToniqDropdown.tagName, () => {
 
     createElementRegistrationTest(ToniqDropdown);
 
-    it('should correctly set default option', async () => {
-        const rendered = await fixture(
-            html`
-                <${ToniqDropdown} ${assign(ToniqDropdown.props.options, options)} />
-            `,
-        );
+    it(
+        'should correctly set default option',
+        createFixtureTest(async () => {
+            const rendered = await fixture(
+                html`
+                    <${ToniqDropdown} ${assign(ToniqDropdown.props.options, options)} />
+                `,
+            );
 
-        const selectedOption = queryThroughShadow('span.select-selected', rendered);
-        expect(selectedOption).to.have.text(options[0].label);
-    });
+            const selectedOption = queryThroughShadow('span.select-selected', rendered);
+            assertInstanceOf(selectedOption, HTMLElement);
+            assert.strictEqual(selectedOption.innerText, options[0].label);
+        }),
+    );
 
-    it('should correctly set selected default option', async () => {
-        const rendered = await fixture(
-            html`
-                <${ToniqDropdown}
-                    ${assign(ToniqDropdown.props.options, options)}
-                    ${assign(ToniqDropdown.props.selected, options[2])}
-                />
-            `,
-        );
+    it(
+        'should correctly set selected default option',
+        createFixtureTest(async () => {
+            const rendered = await fixture(
+                html`
+                    <${ToniqDropdown}
+                        ${assign(ToniqDropdown.props.options, options)}
+                        ${assign(ToniqDropdown.props.selected, options[2])}
+                    />
+                `,
+            );
 
-        const selectedOption = queryThroughShadow('span.select-selected', rendered);
-        expect(selectedOption).to.have.text(options[2].label);
-    });
+            const selectedOption = queryThroughShadow('span.select-selected', rendered);
+            assertInstanceOf(selectedOption, HTMLElement);
+            assert.strictEqual(selectedOption.innerText, options[2].label);
+        }),
+    );
 
-    it('should display the correct number of options', async () => {
-        const rendered = await fixture(
-            html`
-                <${ToniqDropdown} ${assign(ToniqDropdown.props.options, options)} />
-            `,
-        );
+    it(
+        'should display the correct number of options',
+        createFixtureTest(async () => {
+            const rendered = await fixture(
+                html`
+                    <${ToniqDropdown} ${assign(ToniqDropdown.props.options, options)} />
+                `,
+            );
 
-        const optionList = queryThroughShadow('div.select-options', rendered);
-        expect(optionList?.childElementCount).equals(options.length);
-    });
+            const optionList = queryThroughShadow('div.select-options', rendered);
+            assert.strictEqual(optionList?.childElementCount, options.length);
+        }),
+    );
 
     createFocusTests(
-        html`<${ToniqDropdown} ${assign(ToniqDropdown.props.options, [
-            {
-                label: 'test',
-                value: 'test',
-            },
-        ])}></${ToniqDropdown}>`,
+        html`<${ToniqDropdown}
+            ${assign(ToniqDropdown.props.options, [
+                {
+                    label: 'test',
+                    value: 'test',
+                },
+            ])}
+        ></${ToniqDropdown}>`,
         true,
     );
 });
