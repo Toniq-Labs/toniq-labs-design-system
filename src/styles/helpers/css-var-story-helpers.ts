4d551eed5132c05dd1aee97e9b0cb2a231498dc8
electrovir <electrovir@users.noreply.github.com>
2022-06-15 13:59:43 +0000

    convince git that font file names were changed

--- src/styles/helpers/css-var-story-helpers.ts
+++
@@ -1,6 +0,0 @@
-export function createCssVarMap(value: string, cssVars: string[]) {
-    return cssVars.reduce((accum, currentCssVarName) => {
-        accum[currentCssVarName] = value;
-        return accum;
-    }, {} as Record<string, string>);
-}
