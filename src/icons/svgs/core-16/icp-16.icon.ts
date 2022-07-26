d267ca4360342af62bf771d5bc4b4b65dfee56e4
electrovir <electrovir@users.noreply.github.com>
2022-07-26 15:49:27 +0000

    fix icp icon dimensions

--- src/icons/svgs/core-16/icp-16.icon.ts
+++ src/icons/svgs/core-16/icp-16.icon.ts
@@ -1,5 +1,5 @@
 import {html, ToniqSvg} from '../../toniq-svg';
-import {hugeIcpImageData} from '../huge-icp-image-data';
+import {icpSvgImage, icpSvgImageViewBox} from '../icp-svg-image';
 
 export const Icp16Icon = new ToniqSvg(
     'Icp16Icon',
@@ -10,20 +10,9 @@ export const Icp16Icon = new ToniqSvg(
             width="16"
             height="16"
             fill="none"
-            viewBox="0 0 16 16"
+            viewBox="${icpSvgImageViewBox}"
         >
-            <path fill="url(#icp-16__a)" d="M0 0h16v16H0z" />
-            <defs>
-                <pattern
-                    id="icp-16__a"
-                    width="1"
-                    height="1"
-                    patternContentUnits="objectBoundingBox"
-                >
-                    <use xlink:href="#icp-16__b" transform="scale(.0005)" />
-                </pattern>
-                <image xlink:href="${hugeIcpImageData}" id="icp-16__b" width="2000" height="2000" />
-            </defs>
+            ${icpSvgImage}
         </svg>
     `,
 );
--- src/icons/svgs/core-24/icp-24.icon.ts
+++ src/icons/svgs/core-24/icp-24.icon.ts
@@ -1,5 +1,5 @@
 import {html, ToniqSvg} from '../../toniq-svg';
-import {hugeIcpImageData} from '../huge-icp-image-data';
+import {icpSvgImage, icpSvgImageViewBox} from '../icp-svg-image';
 
 export const Icp24Icon = new ToniqSvg(
     'Icp24Icon',
@@ -10,20 +10,10 @@ export const Icp24Icon = new ToniqSvg(
             width="24"
             height="24"
             fill="none"
-            viewBox="0 0 24 24"
+            viewBox="${icpSvgImageViewBox}"
         >
-            <path fill="url(#icp-24__a)" d="M0 0h24v24H0z" />
-            <defs>
-                <pattern
-                    id="icp-24__a"
-                    width="1"
-                    height="1"
-                    patternContentUnits="objectBoundingBox"
-                >
-                    <use xlink:href="#icp-24__b" transform="scale(.0005)" />
-                </pattern>
-                <image xlink:href="${hugeIcpImageData}" id="icp-24__b" width="2000" height="2000" />
-            </defs>
+            ${icpSvgImage}
+        </svg>
         </svg>
     `,
 );
