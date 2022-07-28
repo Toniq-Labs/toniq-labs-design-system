50b6a94128f9a7ea2700595d68f75127be394939
electrovir <electrovir@users.noreply.github.com>
2022-07-28 02:46:04 +0000

    update shadow styles

--- src/styles/shadows.ts
+++ src/styles/shadows.ts
@@ -5,6 +5,6 @@ import {toniqColors} from './colors';
 
 export const toniqShadows = wrapTypeWithReadonly<Record<string, CSSResult>>()({
     popupShadow: css`
-        filter: drop-shadow(0px 16px 16px ${toniqColors.dropShadow.backgroundColor});
+        filter: drop-shadow(0px 12px 8px ${toniqColors.dropShadow.backgroundColor});
     `,
 });
