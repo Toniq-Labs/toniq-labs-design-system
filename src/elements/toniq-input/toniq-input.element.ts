1b3666338dceb12631d3d048fceebf5a90091e80
ponnexcodev <emmanuelr@codev.com>
2022-10-04 13:14:02 +0800

    Updated keypress event comment

--- src/elements/toniq-input/toniq-input.element.ts
+++ src/elements/toniq-input/toniq-input.element.ts
@@ -107,7 +107,7 @@ export const ToniqInput = defineToniqElement<{
          * that was blocked out of programmatic "value" property assignments.
          */
         inputBlocked: defineElementEvent<string>(),
-        /** Fires when a key is press. Useful for triggering events like on key Enter and other keys. */
+        /** Fires when a key is pressed. Useful for triggering events like on key Enter and other keys. */
         keyPress: defineElementEvent<string>(),
     },
     styles: css`
