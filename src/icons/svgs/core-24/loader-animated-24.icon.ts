937d12fd31b9237f79055ed4277f78538c5e9a9c
electrovir <electrovir@users.noreply.github.com>
2023-03-16 00:45:33 +0000

    modify loading spinner so it doesn't gobble up all the CPU

--- src/icons/svgs/core-24/loader-animated-24.icon.ts
+++ src/icons/svgs/core-24/loader-animated-24.icon.ts
@@ -5,50 +5,17 @@ export const LoaderAnimated24Icon = new ToniqSvg(
     'LoaderAnimated24Icon',
     html`
         <style>
-            @keyframes border-jump {
+            @keyframes spin {
                 0% {
-                    opacity: 1;
+                    transform: rotate(0deg);
                 }
-                12.49% {
-                    opacity: 1;
-                }
-                12.5% {
-                    opacity: 1;
-                }
-                40% {
-                    opacity: 0.3;
-                }
-                95% {
-                    opacity: 0;
+                100% {
+                    transform: rotate(360deg);
                 }
             }
 
-            .loader-animated-24-icon__svg path:nth-child(0) {
-                animation: border-jump 1000ms 0ms infinite linear;
-            }
-            .loader-animated-24-icon__svg path:nth-child(1) {
-                animation: border-jump 1000ms 125ms infinite linear;
-            }
-            .loader-animated-24-icon__svg path:nth-child(2) {
-                animation: border-jump 1000ms 250ms infinite linear;
-            }
-            .loader-animated-24-icon__svg path:nth-child(3) {
-                animation: border-jump 1000ms 375ms infinite linear;
-            }
-            .loader-animated-24-icon__svg path:nth-child(4) {
-                animation: border-jump 1000ms 500ms infinite linear;
-            }
-            .loader-animated-24-icon__svg path:nth-child(5) {
-                animation: border-jump 1000ms 625ms infinite linear;
-            }
-            .loader-animated-24-icon__svg path:nth-child(6) {
-                animation: border-jump 1000ms 750ms infinite linear;
-            }
-            .loader-animated-24-icon__svg path:nth-child(7) {
-                animation: border-jump 1000ms 875ms infinite linear;
-            }
-            .loader-animated-24-icon__svg path:nth-child(8) {
-                animation: border-jump 1000ms 1000ms infinite linear;
+            svg {
+                animation: spin 1s infinite linear;
             }
         </style>
         <svg
@@ -60,18 +27,19 @@ export const LoaderAnimated24Icon = new ToniqSvg(
             height="24"
             fill="none"
             stroke="#000"
+            stroke-width="2"
             stroke-linecap="round"
             stroke-linejoin="round"
             viewBox="0 0 24 24"
         >
-            <path stroke-width="2" opacity="0" d="M12 6V3" />
-            <path stroke-width="2" opacity="0" d="M16.25 7.75 18.4 5.6" />
-            <path stroke-width="2" opacity="0" d="M18 12h3" />
-            <path stroke-width="2" opacity="0" d="m16.25 16.25 2.15 2.15" />
-            <path stroke-width="2" opacity="0" d="M12 18v3" />
-            <path stroke-width="2" opacity="0" d="M7.75 16.25 5.6 18.4" />
-            <path stroke-width="2" opacity="0" d="M6 12H3" />
-            <path stroke-width="2" opacity="0" d="M7.75 7.75 5.6 5.6" />
+            <path d="M12 6V3" />
+            <path d="M16.25 7.75 18.4 5.6" />
+            <path d="M18 12h3" />
+            <path d="m16.25 16.25 2.15 2.15" />
+            <path d="M12 18v3" />
+            <path d="M7.75 16.25 5.6 18.4" />
+            <path d="M6 12H3" />
+            <path d="M7.75 7.75 5.6 5.6" />
         </svg>
     `,
 );
