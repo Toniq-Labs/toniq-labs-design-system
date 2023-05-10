3ff10dca2182dbb09741f100e179d3c15403a9c9
electrovir <electrovir@users.noreply.github.com>
2023-05-10 23:24:26 +0000

    move hidden toniq-icon host class to hostClasses

--- packages/design-system/src/elements/toniq-icon/toniq-icon.element.ts
+++ packages/design-system/src/elements/toniq-icon/toniq-icon.element.ts
@@ -4,16 +4,23 @@ import {defineToniqElement} from '../define-toniq-element';
 
 export const ToniqIcon = defineToniqElement<{icon?: ToniqSvg | undefined}>()({
     tagName: 'toniq-icon',
-    styles: css`
+    hostClasses: {
+        'icon-fit': false,
+    },
+    styles: ({hostClassSelectors}) => css`
         :host {
             display: block;
         }
         svg {
-            /* svg is set to inline by default which gives it space under the image. See: https://stackoverflow.com/a/34952703 */
+            /*
+                svg elements are set to inline by default which gives it space under the image. So 
+                we're changing it to block here.
+                See: https://stackoverflow.com/a/34952703
+            */
             display: block;
         }
 
-        :host(.toniq-icon-fit-icon) svg {
+        ${hostClassSelectors['icon-fit']} svg {
             height: 100%;
             width: 100%;
         }
