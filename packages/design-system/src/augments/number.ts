1c4e75a002656e7ff09e5406ee5d9b823d12b427
electrovir <electrovir@users.noreply.github.com>
2023-12-19 13:50:39 +0000

    remove unused mapRange function

--- packages/design-system/src/augments/number.ts
+++ packages/design-system/src/augments/number.ts
@@ -1,18 +1,3 @@
-/**
- * This function re-maps an input in a range of another min and max. If the input min and max is
- * from 0 to 100 and we want an output value that will range from 0 to 500 regardless of the input
- * value. https://www.arduino.cc/reference/en/language/functions/math/map/
- */
-export function mapRange(
-    value: number,
-    inMin: number,
-    inMax: number,
-    outMin: number,
-    outMax: number,
-) {
-    return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-}
-
 export function findClosestRangeIndex(logRange: number[], findValue: number): number {
     const lowestValue = logRange[0];
     const highestValue = logRange[logRange.length - 1];
