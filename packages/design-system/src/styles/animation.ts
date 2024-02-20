ae137c53baeb5cccdfeebf9be84b2294f4fd055a
electrovir <electrovir@users.noreply.github.com>
2024-02-20 01:06:11 +0000

    [minor] add toniqAnimations

--- packages/design-system/src/styles/animation.ts
+++ packages/design-system/src/styles/animation.ts
@@ -1,4 +1,4 @@
-import {css} from 'element-vir';
+import {CSSResult, css} from 'element-vir';
 
 export const durationCssVarNames = {
     interactionDuration: css`--toniq-interaction-transition-duration`,
@@ -15,3 +15,39 @@ export const toniqDurations = {
     /** A longer duration to emphasize the animation. */
     pretty: css`var(${durationCssVarNames.prettyDuration}, 300ms)`,
 };
+
+export type ToniqAnimationDefinition = {animationName: CSSResult; keyFrames: CSSResult};
+
+export const toniqAnimations = {
+    modalGrow: {
+        animationName: css`toniq-modal-grow`,
+        keyFrames: css`
+            @keyframes toniq-modal-grow {
+                0% {
+                    transform: scale(0);
+                }
+                50% {
+                    transform: scale(1.1);
+                }
+                100% {
+                    transform: scale(1);
+                }
+            }
+        `,
+    },
+    menuGrow: {
+        animationName: css`toniq-menu-grow`,
+        keyFrames: css`
+            @keyframes toniq-menu-grow {
+                0% {
+                    transform: scale(0.95);
+                    opacity: 0.6;
+                }
+                100% {
+                    transform: scale(1);
+                    opacity: 1;
+                }
+            }
+        `,
+    },
+} satisfies Record<string, ToniqAnimationDefinition>;
