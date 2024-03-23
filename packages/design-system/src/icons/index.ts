58f89661bfb8c6315bffa3ec3fa69bc03f83503b
Crisdell James Ines <crisdelli@codev.com>
2024-03-23 09:05:36 +0800

    Add Fire 24 icon

--- packages/design-system/src/icons/index.ts
+++ packages/design-system/src/icons/index.ts
@@ -35,6 +35,7 @@ import {Edit24Icon} from './svgs/core-24/edit-24.icon';
 import {Envelope24Icon} from './svgs/core-24/envelope-24.icon';
 import {ExternalLink24Icon} from './svgs/core-24/external-link-24.icon';
 import {Filter24Icon} from './svgs/core-24/filter-24.icon';
+import {Fire24Icon} from './svgs/core-24/fire-24.icon';
 import {Gear24Icon} from './svgs/core-24/gear-24.icon';
 import {Geometry24Icon} from './svgs/core-24/geometry-24.icon';
 import {GridDots24Icon} from './svgs/core-24/grid-dots-24.icon';
@@ -270,6 +271,7 @@ export const allIconsByCategory = {
         Square24Icon,
         SquareCheck24Icon,
         Tag24Icon,
+        Fire24Icon,
         Trash24Icon,
         Upload24Icon,
         Wallet24Icon,
