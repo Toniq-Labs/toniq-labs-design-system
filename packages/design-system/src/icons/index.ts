a7f1cb6f52c53ac02c220f6093ff30fdb4277fd9
ponnexcodev <emmanuelr@codev.com>
2024-04-24 20:35:30 -0400

    Added Shooting Star 16 Icon

--- packages/design-system/src/icons/index.ts
+++ packages/design-system/src/icons/index.ts
@@ -5,6 +5,7 @@ import {CloseX16Icon} from './svgs/core-16/close-x-16.icon';
 import {Copy16Icon} from './svgs/core-16/copy-16.icon';
 import {ExternalLink16Icon} from './svgs/core-16/external-link-16.icon';
 import {InfoCircle16Icon} from './svgs/core-16/info-circle-16.icon';
+import {ShootingStar16Icon} from './svgs/core-16/shooting-star-16.icon';
 import {ArrowBarUp24Icon} from './svgs/core-24/arrow-bar-up-24.icon';
 import {ArrowForwardUp24Icon} from './svgs/core-24/arrow-forward-up-24.icon';
 import {ArrowLeft24Icon} from './svgs/core-24/arrow-left-24.icon';
@@ -124,6 +125,7 @@ export * from './svgs/core-16/close-x-16.icon';
 export * from './svgs/core-16/copy-16.icon';
 export * from './svgs/core-16/external-link-16.icon';
 export * from './svgs/core-16/info-circle-16.icon';
+export * from './svgs/core-16/shooting-star-16.icon';
 export * from './svgs/core-24/arrow-bar-up-24.icon';
 export * from './svgs/core-24/arrow-forward-up-24.icon';
 export * from './svgs/core-24/arrow-left-24.icon';
@@ -247,6 +249,7 @@ export const allIconsByCategory = {
         Copy16Icon,
         ExternalLink16Icon,
         InfoCircle16Icon,
+        ShootingStar16Icon,
     ],
     'core-24': [
         ArrowBarUp24Icon,
---
+++ packages/design-system/src/icons/svgs/core-16/shooting-star-16.icon.ts
@@ -0,0 +1,22 @@
+import {html} from 'element-vir';
+import {defineIcon} from 'vira';
+import {toniqIconCssVars} from '../../../styles/icon-css-vars';
+
+export const ShootingStar16Icon = defineIcon({
+    name: 'ShootingStar16Icon',
+    svgTemplate: html`
+        <svg
+            width="16"
+            height="16"
+            viewBox="0 0 16 16"
+            fill=${toniqIconCssVars['toniq-icon-fill-color'].value}
+            stroke=${toniqIconCssVars['toniq-icon-stroke-color'].value}
+            stroke-width=${toniqIconCssVars['toniq-icon-stroke-width'].value}
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="m10.333 3.667-2-1L8.666 5 7.333 6.333l2 .333 1 2 1-2 2-.333L12 5l.333-2.333zm-7.666 9.667 4.667-4.667M6 13.334 8.333 11m-5.666-1L5 7.667"
+            />
+        </svg>
+    `,
+});
