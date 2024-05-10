62af2b9715a18704b5293aa25b695dc4485e25cd
Crisdell James Ines <crisdelli@codev.com>
2024-05-10 10:14:01 +0800

    Update ToniqError to use ToniqAlert.Danger

--- packages/design-system/src/elements/toniq-error-message/toniq-error-message.element.ts
+++ packages/design-system/src/elements/toniq-error-message/toniq-error-message.element.ts
@@ -1,22 +1,19 @@
 import {css, html} from 'element-vir';
 import {defineToniqElementNoInputs} from '../define-toniq-element';
+import {ToniqAlert, ToniqAlertVariantEnum} from '../toniq-alert/toniq-alert.element';
 
 export const ToniqErrorMessage = defineToniqElementNoInputs({
     tagName: 'toniq-error-message',
     styles: css`
         :host {
             display: block;
-            color: red;
-        }
-
-        p {
-            padding: 0;
-            margin: 0;
         }
     `,
     renderCallback() {
         return html`
-            <p><slot></slot></p>
+            <${ToniqAlert.assign({variant: ToniqAlertVariantEnum.Danger})}>
+                <slot></slot>
+            </${ToniqAlert}>
         `;
     },
 });
