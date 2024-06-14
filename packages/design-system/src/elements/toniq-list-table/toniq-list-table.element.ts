e671879dd18c07d05db6bae472e696d9dbf3626d
ponnexcodev <emmanuelr@codev.com>
2024-06-14 02:05:54 -0400

    Table List Optimization

--- packages/design-system/src/elements/toniq-list-table/toniq-list-table.element.ts
+++ packages/design-system/src/elements/toniq-list-table/toniq-list-table.element.ts
@@ -18,6 +18,7 @@ import {
     onResize,
     perInstance,
     renderIf,
+    repeat,
     unsafeCSS,
 } from 'element-vir';
 import {ChevronsRight16Icon} from '../../icons/svgs/core-16/chevrons-right-16.icon';
@@ -426,85 +427,93 @@ export const ToniqListTable = defineToniqElement<ListTableInputs>()({
                           })
                         : nothing}
                 >
-                    ${enabledColumns.map((item, index) => {
-                        const contents = row.cells[item.key as keyof typeof row];
+                    ${repeat(
+                        enabledColumns,
+                        (item, index) => index,
+                        (item, index) => {
+                            const contents = row.cells[item.key as keyof typeof row];
 
-                        const rowItemStyle = css`
-                            left: ${unsafeCSS(`${state.rowStyles[item.key as string]?.left}px`)};
-                            min-width: ${index >= enabledColumns.length - 1
-                                ? unsafeCSS('unset')
-                                : unsafeCSS(`${state.rowStyles[item.key as string]?.width}px`)};
-                        `;
-                        return html`
-                            <div
-                                class=${classMap({
-                                    'row-item': true,
-                                    sticky: !!item.option?.sticky && state.canScroll,
-                                    fill: !!item.option?.spaceEvenly,
-                                })}
-                                style=${rowItemStyle}
-                            >
+                            const rowItemStyle = css`
+                                left: ${unsafeCSS(
+                                    `${state.rowStyles[item.key as string]?.left}px`,
+                                )};
+                                min-width: ${index >= enabledColumns.length - 1
+                                    ? unsafeCSS('unset')
+                                    : unsafeCSS(`${state.rowStyles[item.key as string]?.width}px`)};
+                            `;
+                            return html`
                                 <div
                                     class=${classMap({
-                                        'row-content': true,
+                                        'row-item': true,
+                                        sticky: !!item.option?.sticky && state.canScroll,
+                                        fill: !!item.option?.spaceEvenly,
                                     })}
-                                    ${onResize((event) => {
-                                        const container = event.target;
-                                        if (!(container instanceof HTMLElement)) {
-                                            throw new Error('onResize event had no target');
-                                        }
-
-                                        const parentEl = container.closest('.table-list');
-                                        const containerLeft =
-                                            parentEl?.getBoundingClientRect().left;
-
-                                        const rowItem = parentEl?.querySelectorAll('.row-item')[
-                                            index
-                                        ] as HTMLElement;
-                                        const left = rowItem?.getBoundingClientRect().left;
-
-                                        const currentWidth =
-                                            container.getBoundingClientRect().width;
-                                        if (
-                                            !state.rowStyles[item.key as string]?.width ||
-                                            currentWidth >
-                                                (state.rowStyles[item.key as string]
-                                                    ?.width as number)
-                                        ) {
-                                            updateState({
-                                                rowStyles: {
-                                                    ...state.rowStyles,
-                                                    [item.key]: {
-                                                        width: currentWidth,
-                                                        left: containerLeft
-                                                            ? left - containerLeft
-                                                            : left,
-                                                    },
-                                                },
-                                            });
-                                        }
-                                        if (!inputs.nonBlocking) {
-                                            updateState({
-                                                itemsPainted: state.itemsPainted + 1,
-                                            });
-                                        }
-                                    })}
+                                    style=${rowItemStyle}
                                 >
-                                    ${renderIf(
-                                        rowIndex === 0,
-                                        html`
-                                            <span class="header" style=${item.style}>
-                                                ${item.title}
-                                            </span>
-                                        `,
-                                        html`
-                                            ${contents}
-                                        `,
-                                    )}
+                                    <div
+                                        class=${classMap({
+                                            'row-content': true,
+                                        })}
+                                        ${onResize((event) => {
+                                            setTimeout(() => {
+                                                const container = event.target;
+                                                if (!(container instanceof HTMLElement)) {
+                                                    throw new Error('onResize event had no target');
+                                                }
+
+                                                const parentEl = container.closest('.table-list');
+                                                const containerLeft =
+                                                    parentEl?.getBoundingClientRect().left;
+
+                                                const rowItem = parentEl?.querySelectorAll(
+                                                    '.row-item',
+                                                )[index] as HTMLElement;
+                                                const left = rowItem?.getBoundingClientRect().left;
+
+                                                const currentWidth =
+                                                    container.getBoundingClientRect().width;
+                                                if (
+                                                    !state.rowStyles[item.key as string]?.width ||
+                                                    currentWidth >
+                                                        (state.rowStyles[item.key as string]
+                                                            ?.width as number)
+                                                ) {
+                                                    updateState({
+                                                        rowStyles: {
+                                                            ...state.rowStyles,
+                                                            [item.key]: {
+                                                                width: currentWidth,
+                                                                left: containerLeft
+                                                                    ? left - containerLeft
+                                                                    : left,
+                                                            },
+                                                        },
+                                                    });
+                                                }
+                                                if (!inputs.nonBlocking) {
+                                                    updateState({
+                                                        itemsPainted: state.itemsPainted + 1,
+                                                    });
+                                                }
+                                            }, 0);
+                                        })}
+                                    >
+                                        ${renderIf(
+                                            rowIndex === 0,
+                                            html`
+                                                <span class="header" style=${item.style}>
+                                                    ${item.title}
+                                                </span>
+                                            `,
+                                            html`
+                                                ${contents}
+                                            `,
+                                        )}
+                                    </div>
                                 </div>
-                            </div>
-                        `;
-                    })}
+                            `;
+                        },
+                    )}
                 </div>
             `;
         }
@@ -538,11 +547,15 @@ export const ToniqListTable = defineToniqElement<ListTableInputs>()({
                         }
                     })}
                 >
-                    ${rows.filter(isTruthy).map((item: ListTableRow<any>, index: number) => {
-                        return html`
-                            ${listItem(item, index)}
-                        `;
-                    })}
+                    ${repeat(
+                        rows,
+                        (item, index) => index,
+                        (item: ListTableRow<any>, index: number) => {
+                            return html`
+                                ${listItem(item, index)}
+                            `;
+                        },
+                    )}
                     ${renderIf(
                         state.canScroll,
                         html`
