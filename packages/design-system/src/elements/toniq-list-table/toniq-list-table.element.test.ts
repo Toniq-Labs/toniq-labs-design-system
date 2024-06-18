d5ed829e379b5b958f549205375ff4e28d9d39ef
ponnexcodev <emmanuelr@codev.com>
2024-06-18 02:49:23 -0400

    remove renderTest

--- packages/design-system/src/elements/toniq-list-table/toniq-list-table.element.test.ts
+++ packages/design-system/src/elements/toniq-list-table/toniq-list-table.element.test.ts
@@ -1,8 +1,6 @@
-import {itCases} from '@augment-vir/browser-testing';
 import {fixture as renderFixture} from '@open-wc/testing';
 import {html} from 'element-vir';
-import {assertInstanceOf, assertTypeOf} from 'run-time-assertions';
-import {ToniqPagination} from '../toniq-pagination/toniq-pagination.element';
+import {assertTypeOf} from 'run-time-assertions';
 import {ToniqListTable, createListTableTable} from './toniq-list-table.element';
 
 const exampleListTableInputs = createListTableTable({
@@ -38,56 +36,6 @@ describe(ToniqListTable.tagName, () => {
             })}></${ToniqListTable}>
         `);
     });
-
-    async function testRender(testCase: {inputs: typeof ToniqListTable.inputsType}) {
-        const renderedToniqListTable = await renderFixture(html`
-            <${ToniqListTable.assign(testCase.inputs)}></${ToniqListTable}>
-        `);
-        assertInstanceOf(renderedToniqListTable, ToniqListTable);
-        const renderedRows = renderedToniqListTable.shadowRoot.querySelectorAll('.row-wrapper');
-
-        if (testCase.inputs.pagination) {
-            assertInstanceOf(renderedToniqListTable, ToniqListTable);
-            const renderedToniqPagination = renderedToniqListTable.shadowRoot.querySelector(
-                ToniqPagination.tagName,
-            );
-            assertInstanceOf(renderedToniqPagination, ToniqPagination);
-        }
-
-        return {
-            // Minus the header row
-            row: renderedRows.length - 1,
-        };
-    }
-
-    itCases(testRender, [
-        {
-            it: 'renders correct number of row items',
-            input: {
-                inputs: {
-                    ...exampleListTableInputs,
-                },
-            },
-            expect: {
-                row: 2,
-            },
-        },
-        {
-            it: 'renders pagination',
-            input: {
-                inputs: {
-                    ...exampleListTableInputs,
-                    pagination: {
-                        currentPage: 3,
-                        pageCount: 10,
-                    },
-                },
-            },
-            expect: {
-                row: 2,
-            },
-        },
-    ]);
 });
 
 describe(createListTableTable.name, () => {
