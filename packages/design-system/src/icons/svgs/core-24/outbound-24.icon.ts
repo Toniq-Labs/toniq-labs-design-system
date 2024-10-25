0c60d8bc5650e591e5dd47066fd084284d16719b
Crisdell James Ines <crisdelli@codev.com>
2024-10-25 13:31:37 +0800

    Add outbound icon

---
+++ packages/design-system/src/icons/svgs/core-24/outbound-24.icon.ts
@@ -0,0 +1,23 @@
+import {html} from 'element-vir';
+import {defineIcon} from 'vira';
+import {toniqIconCssVars} from '../../../styles';
+
+export const Outbound24Icon = defineIcon({
+    name: 'Outbound24',
+    svgTemplate: html`
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill=${toniqIconCssVars['toniq-icon-fill-color'].value}
+            stroke=${toniqIconCssVars['toniq-icon-stroke-color'].value}
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            width="24"
+            height="24"
+            stroke-width="2"
+        >
+            <path d="M3 12a9 9 0 1 0 18 0 9 9 0 1 0-18 0m6 3 6-6" />
+            <path d="M11 9h4v4" />
+        </svg>
+    `,
+});
