b86784f60ca7c165ce6a0b1474e3158e20578aed
Crisdell James Ines <crisdelli@codev.com>
2024-10-26 06:06:10 +0800

    Fix icon rotation on safari

--- packages/design-system/src/icons/svgs/core-24/inbound-24.icon.ts
+++ packages/design-system/src/icons/svgs/core-24/inbound-24.icon.ts
@@ -15,10 +15,9 @@ export const Inbound24Icon = defineIcon({
             width="24"
             height="24"
             stroke-width="2"
-            transform="rotate(90)"
         >
-            <path d="M3 12a9 9 0 1 0 18 0 9 9 0 1 0-18 0m6 3 6-6" />
-            <path d="M11 9h4v4" />
+            <path transform="rotate(90 12 12)" d="M3 12a9 9 0 1 0 18 0 9 9 0 1 0-18 0m6 3 6-6" />
+            <path transform="rotate(90 12 12)" d="M11 9h4v4" />
         </svg>
     `,
 });
