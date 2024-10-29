00ff30661f2f076bfde3c4ee72ac8d0d000286aa
ponnexcodev <99699764+ponnexcodev@users.noreply.github.com>
2024-10-29 18:37:20 -0400

    [patch][v16.26.1]Table List Styling Fixes (#150)

--- packages/design-system/src/elements/toniq-list-table/toniq-list-table.element.ts
+++ packages/design-system/src/elements/toniq-list-table/toniq-list-table.element.ts
@@ -160,7 +160,7 @@ export const ToniqListTable = defineToniqElement<ListTableInputs>()({
 
         .table-wrapper {
             position: relative;
-            overflow-x: auto;
+            overflow: auto hidden;
             display: flex;
             flex-grow: 1;
             flex-direction: column;
@@ -176,8 +176,7 @@ export const ToniqListTable = defineToniqElement<ListTableInputs>()({
             width: 100%;
             display: flex;
             flex-direction: row;
-            overflow-x: auto;
-            overflow-y: hidden;
+            overflow: auto hidden;
             scrollbar-width: thin;
             scrollbar-color: ${scrollbarColorCssVar} ${scrollbarTrackColorCssVar};
         }
@@ -414,7 +413,7 @@ export const ToniqListTable = defineToniqElement<ListTableInputs>()({
                                         : nothing}
                                     class=${classMap({
                                         'column-content': true,
-                                        hover: rowIndex === state.hoverIndex,
+                                        hover: rowIndex !== 0 && rowIndex === state.hoverIndex,
                                     })}
                                 >
                                     ${renderIf(
